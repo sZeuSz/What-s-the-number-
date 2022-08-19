39befa99e9421146b7b78bfcb43ad881917ed714
sZeuSz <rosenosilva@super.ufam.edu.br>
2022-08-18 21:43:27 -0400

    feature(form): add validations in form to number choiced

---
+++ js/data.js
@@ -0,0 +1,41 @@
+export let leds = [
+  `<div class="main unit ">
+      <div class="position-with-border top"></div>
+      <div class="position-with-border left-led-first"></div>
+      <div class="center-led "><div class="center ">
+          <div class="arrow-right"></div>
+          <div class="arrow-left"></div>
+      </div></div>
+      <div class="position-with-border left-led-second"></div>
+      <div class="position-with-border right-led-first"></div>
+      <div class="position-with-border right-led-second"></div>
+      <div class="position-with-border bottom"></div>
+  </div>
+  `,
+  `<div class="main hundred ">
+      <div class="position-with-border top"></div>
+      <div class="position-with-border left-led-first"></div>
+      <div class="center-led "><div class="center">
+          <div class="arrow-right"></div>
+          <div class="arrow-left"></div>
+      </div></div>
+      <div class="position-with-border left-led-second"></div>
+      <div class="position-with-border right-led-first"></div>
+      <div class="position-with-border right-led-second"></div>
+      <div class="position-with-border bottom"></div>
+   </div>`,
+  `
+  <div class="main dozen ">
+    <div class="position-with-border top"></div>
+    <div class="position-with-border left-led-first"></div>
+    <div class="center-led "><div class="center">
+        <div class="arrow-right"></div>
+        <div class="arrow-left"></div>
+    </div></div>
+    <div class="position-with-border left-led-second"></div>
+    <div class="position-with-border right-led-first"></div>
+    <div class="position-with-border right-led-second"></div>
+    <div class="position-with-border bottom"></div>
+  </div>
+  `,
+];
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,50 @@
-function submitAnswer() {
+import { leds } from "./data.js";
+
+let validNumber = false;
+
+function submitAnswer(numberchoiced, numbermagic) {
   console.log("lógica de submissão");
+
+  let digits = 0;
+  while (numberchoiced > 0) {
+    numberchoiced = Math.floor(numberchoiced / 10);
+    digits += 1;
+  }
+
+  let panel = document.querySelector(".panel");
+  panel.innerHTML = "";
+  for (let i = 0; i < digits; i++) {
+    panel.innerHTML += leds[i];
+  }
 }
 
 function preventDefault(evt) {
   evt.preventDefault();
-  console.log(evt);
-  submitAnswer();
+
+  let numberchoiced = Number(document.querySelector(".input").value);
+
+  if (validNumber) {
+    //lógica requisição
+    submitAnswer(numberchoiced);
+  }
 }
+
+const handleInput = (event) => {
+  const { value } = event.target;
+
+  let errolabel = document.querySelector(".erro-info");
+
+  if (!(value > 0 && value < 300)) {
+    errolabel.innerHTML = "O número deve estar entre 0 e 300";
+    validNumber = false;
+    return;
+  }
+
+  errolabel.innerHTML = "";
+  validNumber = true;
+};
 
 document
   .querySelector(".submit")
   .addEventListener("click", preventDefault, false);
+document.querySelector(".input").addEventListener("keyup", handleInput, false);
